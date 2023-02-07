35fe0fcda9ef3f7be4e9c914a0ee65c1dfdb293f
Saabbir31 <124292298+Saabbir31@users.noreply.github.com>
2023-02-07 15:44:42 +0600

    Add files via upload

---
+++ Employee/src/Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+    int salary;
+    String name;
+    void display()
+    {
+        System.out.println(name+":"+salary);
+    }
+}
+
+
---
+++ Employee/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee s= new Employee();
+        s.salary=600000;
+        s.name="Sabbir";
+        s.display();
+    }
+}
+
