2df63b29f37c75b591126d5efbfdcb4277c69d6d
Saabbir31 <124292298+Saabbir31@users.noreply.github.com>
2023-02-07 12:18:24 +0600

    Add to another file

---
+++ just print/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    int x = 3;
+    float a=7.1234f;
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println(myObj.x+"\n"+myObj.a);
+
+    }
+}
